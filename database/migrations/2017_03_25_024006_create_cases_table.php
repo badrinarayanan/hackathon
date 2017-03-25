f40c8b90c48199842b2adb0c35a42a8e18b2fbd6
= <=>
2017-03-25 09:06:33 +0530

    First commit

---
+++ database/migrations/2017_03_25_024006_create_cases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer("patient_id");
+            $table->longtext("patient_test");
+            $table->string("prediction");
+            $table->string("diagnosis");
+            $table->string("doctor_comments");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cases');
+    }
+}
